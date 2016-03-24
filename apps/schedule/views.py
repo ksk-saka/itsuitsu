0aa40812069db7e717414a6b9ed63f27dd44900f
ksk-saka <ksk.skmt@gmail.com>
2016-03-25 02:10:25 +0900

    fix #5 とりあえずの登録/編集フォームを作った

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import ScheduleForm
 from .models import Schedule
-from django.core.exceptions import *
+
 
 def index(request):
     """ index
@@ -11,16 +13,29 @@ def index(request):
     :return:
     """
     schedules = Schedule.objects.all()
-    return render(request, 'index.html', {
+    return render(request, 'schedule/index.html', {
         'schedules': schedules,
     })
 
 
-def add(request):
-    """
+class ScheduleCreate(CreateView):
+    """ScheduleCreate
 
-    編集画面です。（仮）
-    :param request:
-    :return:
+    スケジュールを登録します。
     """
-    return render(request, 'edit.html')
+    form_class = ScheduleForm
+    template_name = 'schedule/add.html'
+    success_url = '/schedule/'
+
+
+class ScheduleUpdate(UpdateView):
+    """ScheduleUpdate
+
+    スケジュールを編集します。
+    """
+    form_class = ScheduleForm
+    template_name = 'schedule/edit.html'
+    success_url = '/schedule/'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Schedule, id=self.kwargs['id'])
