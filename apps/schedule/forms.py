60a4243c89e1f80e9c4e84f211e8b1dbfabbd461
ksk-saka <ksk.skmt@gmail.com>
2016-04-09 00:59:41 +0900

    refs #11 日にちの重複チェックを追加した

--- apps/schedule/forms.py
+++ apps/schedule/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.forms import ModelForm
-from django.forms.models import inlineformset_factory
+from django.forms import ModelForm, ValidationError
+from django.forms.models import BaseInlineFormSet, inlineformset_factory
 from apps.schedule.models import Schedule, ScheduleDate
 
 
@@ -35,10 +35,23 @@ class ScheduleDateForm(ModelForm):
             'date': '日にち',
         }
 
+
+class BaseScheduleDateFormSet(BaseInlineFormSet):
+    """BaseScheduleDateFormSet
+
+    スケジュール登録/編集フォームセットです。
+    """
+    def clean(self):
+        dates = [form['date'].value() for form in self.forms if form['date'].value()]
+        if len(dates) > len(set(dates)):
+            raise ValidationError('日にちが重複しています。')
+
+
 ScheduleDateFormSet = inlineformset_factory(
     Schedule,
     ScheduleDate,
     form=ScheduleDateForm,
+    formset=BaseScheduleDateFormSet,
     extra=2,
     can_delete=False,
     min_num=1,
